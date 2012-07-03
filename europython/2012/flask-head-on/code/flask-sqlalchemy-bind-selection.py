dc57049512bf8ee2366bfb44fc7ed2a4f6505244
Andrii Mishkovskyi <gdmfsob@mishkovskyi.net>
2012-07-03 11:52:57 +0200

    Fixed a few examples, updated wording in places

--- europython/2012/flask-head-on/code/flask-sqlalchemy-bind-selection.py
+++ europython/2012/flask-head-on/code/flask-sqlalchemy-bind-selection.py
@@ -1,5 +1,5 @@
 db.session.using_bind('slave').query(...)
-db.session.using_bind('master').query()
+db.session.using_bind('master').query(...)
 
 AdminUser.query_using('admin-slave-1').all()
 AdminUser.query_using('admin-slave-2').all()
--- europython/2012/flask-head-on/code/statsd-example.py
+++ europython/2012/flask-head-on/code/statsd-example.py
@@ -1,10 +1,10 @@
 def setup_statsd(app):
     host = app.config['STATSD_HOST']
     port = app.config['STATSD_PORT']
-    app.statsd = statsd.Client(
-        'ignite', statsd_connection)
     connection = statsd.Connection(
         host=host, port=port, sample_rate=0.1)
+    app.statsd = statsd.Client(
+        'ignite', statsd_connection)
 
 def statsd_metric(metric, duration=None):
     counter = app.statsd.get_client(
